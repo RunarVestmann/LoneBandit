178593d4885524af407d36d8f3ed55b5ba26843c
Rúnar Vestmann <runarvestmann@gmail.com>
2021-11-29 14:11:57 +0000

    Added orb animations

--- Assets/Scripts/Orb.cs
+++ Assets/Scripts/Orb.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class Orb : MonoBehaviour
 {
     [SerializeField] Collider2D orbCollider;
     [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Animator animator;
     [SerializeField] float timeUntilPlayerCanCollectAgain;
     [SerializeField] float fadeInTime;
     [SerializeField] UnityEvent onCollect;
+
+    // Start playing the hover animation at a random offset so orbs don't hover at the exact same rate
+    void Start() => animator.PlayInFixedTime("Hover", 0, Random.Range(0f, 0.3f));
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -22,15 +27,15 @@ public class Orb : MonoBehaviour
     {
         onCollect.Invoke();
         orbCollider.enabled = false;
-        spriteRenderer.enabled = false;
+        animator.Play("Collect");
         yield return new WaitForSeconds(timeUntilPlayerCanCollectAgain);
         orbCollider.enabled = true;
-        spriteRenderer.enabled = true;
         yield return FadeInRoutine();
     }
 
     IEnumerator FadeInRoutine()
     {
+        animator.Play("Hover");
         var elapsedTime = 0f;
         var color = spriteRenderer.color;
 
@@ -41,6 +46,7 @@ public class Orb : MonoBehaviour
             elapsedTime += Time.deltaTime;
             yield return null;
         }
+
         color.a = 1f;
         spriteRenderer.color = color;
     }
