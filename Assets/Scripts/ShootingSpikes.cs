09a0e1301e5446d9282e43b064d69750d22fc261
eggerth19 <eggerth19@ru.is>
2021-11-30 22:09:33 +0000

    collapsible stones

--- Assets/Scripts/ShootingSpikes.cs
+++ Assets/Scripts/ShootingSpikes.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ShootingSpikes : MonoBehaviour
 {
+    [SerializeField] float timeToLive;
     Rigidbody2D rb2d;
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, timeToLive);
     }
 
     public void Shoot(Vector2 direction, float speed)
@@ -18,6 +20,9 @@ public class ShootingSpikes : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("Hit");
+        var player = collider.GetComponent<Player>();
+        if (!player) return;
+        Destroy(gameObject);
     }
+
 }
