02ce28cb8667219f48360ba117a358b88e2401bb
eggerth19 <eggerth19@ru.is>
2021-12-06 13:30:01 +0000

    checkpoint lighting on active

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class Checkpoint : MonoBehaviour
 {
     Animator animator;
+    Light2D light;
 
     [SerializeField] UnityEvent<Vector3> spawnLocationEvent;
     [SerializeField] AudioSource audioSource;
@@ -15,6 +17,7 @@ public class Checkpoint : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
+        light = GetComponentInChildren<Light2D>();
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -23,10 +26,10 @@ public class Checkpoint : MonoBehaviour
         var player = other.GetComponent<Player>();
         if (!player) return;
         animator.Play("Active");
+        light.enabled = true;
         isActive = true;
         audioSource.Play();
         spawnLocationEvent.Invoke(transform.position);
     }
-
 
 }
